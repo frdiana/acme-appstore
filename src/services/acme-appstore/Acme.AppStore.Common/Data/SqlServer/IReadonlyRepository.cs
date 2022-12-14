6cdb18af2c4b4afa1afd5d6f422cb319dac9e365
Francesco Diana <kekko.dia@gmail.com>
2022-12-14 16:50:35 +0100

    Delete src/services/acme-appstore directory

--- src/services/acme-appstore/Acme.AppStore.Common/Data/SqlServer/IReadonlyRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Acme.AppStore.Common.Data.SqlServer;
-
-public interface IReadonlyRepository<TEntity, TKey> where TEntity : class, IEntity<TKey> where TKey : IEquatable<TKey>
-{
-    IEnumerable<TEntity> GetAll();
-    TEntity GetById(TKey id);
-}
-
--- src/services/acme-appstore/Acme.AppStore.Common/Data/SqlServer/IRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Acme.AppStore.Common.Data.SqlServer;
-    public interface IRepository<TEntity, TKey> : IReadonlyRepository<TEntity, TKey> where TEntity : class, IEntity<TKey> where TKey : IEquatable<TKey>
-    {
-
-
-
-    }
-
